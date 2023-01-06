c8d30e5092cf36e07b01abbb886dc804620b1829
poseanop <k3253hj@naver.com>
2023-01-06 20:58:25 +0900

    config

--- docs/intro.md
+++ docs/intro.md
@@ -3,3 +3,5 @@ sidebar_position: 1
 ---
 
 # 개발블로그
+
+[로드맵](https://roadmap.sh/frontend/) 기반으로 일주일에 한개씩 포스팅합니다.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Tech & Writing Blog",
-  tagline: "poseanop",
+  tagline: "hello world!",
   url: "https://poseanop.vercel.app",
   baseUrl: "/",
   onBrokenLinks: "throw",
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "poseanop", // Usually your GitHub org/user name.
+  projectName: "blog", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -36,15 +36,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -71,7 +69,7 @@ const config = {
           },
           { to: "/blog", label: "Writing", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/poseanop/blog",
             label: "GitHub",
             position: "right",
           },
@@ -81,31 +79,15 @@ const config = {
         style: "dark",
         links: [
           {
-            title: "Docs",
+            title: "Tech",
             items: [
               {
-                label: "Tutorial",
+                label: "Index",
                 to: "/docs/intro",
               },
             ],
           },
-          {
-            title: "Community",
-            items: [
-              {
-                label: "Stack Overflow",
-                href: "https://stackoverflow.com/questions/tagged/docusaurus",
-              },
-              {
-                label: "Discord",
-                href: "https://discordapp.com/invite/docusaurus",
-              },
-              {
-                label: "Twitter",
-                href: "https://twitter.com/docusaurus",
-              },
-            ],
-          },
+
           {
             title: "More",
             items: [
